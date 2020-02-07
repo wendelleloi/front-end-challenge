af341993616ea33d56c64cc1ead22b52b6eeb71a
wendelleloi <wendelleloi2@gmail.com>
2020-02-07 00:12:17 -0300

    Adiciona estilo de slider

--- src/components/structure/Carros.vue
+++ src/components/structure/Carros.vue
@@ -6,12 +6,15 @@
     .swiper-slider
       swiper(:options="swiperOption" ref="mySwiper")
         swiper-slide(v-for="(swiper, index) in this.SwiperSliders" :key="index")
-          a(href="#")
+          a(href="#" :title="swiper.label")
             img(:src="swiper.img")
+            span {{swiper.label}}
         .swiper-button-prev(slot="button-prev")
-          i.icone.icon-angle-left
+          .bg-prev
+            .bg
         .swiper-button-next(slot="button-next")
-          i.icone.icon-angle-right
+          .bg-next
+            .bg
         .swiper-pagination(slot="pagination")
 </template>
 
@@ -40,16 +43,16 @@ export default {
         }
       },
       SwiperSliders: [
-        { img: require('../../assets/slider/classeA.svg') },
-        { img: require('../../assets/slider/classeC.svg') },
-        { img: require('../../assets/slider/classeCLA.svg') },
-        { img: require('../../assets/slider/classeC.svg') },
-        { img: require('../../assets/slider/classeA.svg') },
-        { img: require('../../assets/slider/classeA.svg') },
-        { img: require('../../assets/slider/classeC.svg') },
-        { img: require('../../assets/slider/classeCLA.svg') },
-        { img: require('../../assets/slider/classeC.svg') },
-        { img: require('../../assets/slider/classeA.svg') }
+        { img: require('../../assets/slider/classeA.svg'), label: 'Classe A' },
+        { img: require('../../assets/slider/classeC.svg'), label: 'Classe C' },
+        { img: require('../../assets/slider/classeCLA.svg'), label: 'Classe CLA' },
+        { img: require('../../assets/slider/classeC.svg'), label: 'Classe C' },
+        { img: require('../../assets/slider/classeA.svg'), label: 'Classe A' },
+        { img: require('../../assets/slider/classeA.svg'), label: 'Classe A' },
+        { img: require('../../assets/slider/classeC.svg'), label: 'Classe C' },
+        { img: require('../../assets/slider/classeCLA.svg'), label: 'Classe CLA' },
+        { img: require('../../assets/slider/classeC.svg'), label: 'Classe C' },
+        { img: require('../../assets/slider/classeA.svg'), label: 'Classe A' }
       ]
     }
   },
@@ -95,6 +98,64 @@ export default {
   .swiper-slider{
     width: 80%;
     margin: 50px 0;
+    a{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      color: $color-gray-darker;
+      &:hover{
+        text-decoration: none;
+      }
+    }
+    .swiper-pagination-bullet {
+      width: 14px;
+      height: 14px;
+      margin: 0 10px 0 0;
+      opacity: 1;
+      background-color: $color-gray;
+    }
+    .swiper-pagination-bullet-active {
+      background: #b71d3d;
+      border-style: none;
+    }
+    .swiper-container-horizontal{
+      height: 200px;
+    }
+    .swiper-button-prev,.swiper-button-next{
+      width: 30px;
+      height: 30px;
+      background-image: none;
+      .bg{
+        height: 100%;
+        width: 100%;
+        background-color: #000;
+        opacity: 0.2;
+        &:hover{
+          opacity: .5;
+        }
+      }
+    }
+    .swiper-button-next{
+      .bg-next{
+        height: 100%;
+        width: 100%;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-image: url('../../assets/slider/rightArrow.svg');
+        /* filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(259deg) brightness(107%) contrast(103%); */
+      }
+    }
+    .swiper-button-prev{
+      .bg-prev{
+        height: 100%;
+        width: 100%;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-image: url('../../assets/slider/rightArrow.svg');
+        /* filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(259deg) brightness(107%) contrast(103%); */
+        transform: rotate(180deg);
+      }
+    }
     img{
       max-width: 180px;
       max-height: 110px;
